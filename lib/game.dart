eaaf1d43ded1c1d1dbbf65599453912eb36ca290
Styaaa <s160421048@student.ubaya.ac.id>
2024-04-12 20:09:39 +0800

    init

--- lib/game.dart
+++ lib/game.dart
@@ -1,6 +1,29 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class Game extends StatelessWidget{
+class Game extends StatefulWidget{
+  const Game({super.key});
+
+  @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  bool animated = false;
+  late Timer _timer;
+  double opacityLev = 0;
+
+  @override
+  void initState(){
+    super.initState();
+    _timer = Timer.periodic(Duration(milliseconds: 1000), (timer) {
+      setState(() {
+        animated = true;
+        opacityLev = 0 + 1;
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -8,10 +31,34 @@ class Game extends StatelessWidget{
       // appBar: AppBar(
       //   title: Text("History"),
       // ),
-      body: Center(
-        child: Text("This is history."),
+      body: ListView(
+        children: [
+          Container(
+              width: 250.0,
+              height: 250.0,
+              child: AnimatedAlign(
+                alignment: animated ? Alignment.topCenter : Alignment.center,
+                duration: const Duration(seconds: 3),
+                curve: Curves.fastOutSlowIn,
+                child:
+                Text("Get Ready"),
+              )
+          ),
+          Container(
+              width: 250.0,
+              height: 250.0,
+              child: AnimatedOpacity(
+                opacity: opacityLev,
+                duration: const Duration(seconds: 1),
+                child:
+                Image.network('https://loremflickr.com/320/240/cake?lock=9'),
+              )
+          ),
+        ],
+      ),
+      appBar: AppBar(
+        title: const Text('animation test'),
       ),
     );
   }
-
 }
