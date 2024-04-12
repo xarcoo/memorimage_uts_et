fdf966632a99f1cbc7d274d87eae96a1ab47909b
xarcoo <archieoematan21@gmail.com>
2024-04-12 16:55:07 +0700

    PROJEK AWAL

---
+++ README.md
@@ -0,0 +1,3 @@
+# memorimage_uts_et
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/memorimage_uts_et/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.memorimage_uts_et
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
