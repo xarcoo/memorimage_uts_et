9b4d6b2467a4b259a2683ed69f6606dc40b86a8b
xarcoo <archieoematan21@gmail.com>
2024-04-12 17:17:33 +0700

    login

---
+++ lib/login.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:memorimage_uts_et/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+String _username = "";
+
+class LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Login(),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginState();
+  }
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        height: 300,
+        margin: EdgeInsets.all(20),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            border: Border.all(width: 1),
+            color: Colors.white,
+            boxShadow: [BoxShadow(blurRadius: 20)]),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                onChanged: (v) {
+                  _username = v;
+                },
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText:
+                    true, //untuk buat tulisan jadi bintang (bagus untuk password)
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Container(
+                height: 50,
+                width: 300,
+                decoration: BoxDecoration(
+                    // color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20)),
+                child: ElevatedButton(
+                  onPressed: () {
+                    doLogin();
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void doLogin() async {
+  //later, we use web service here to check the user id and password
+  final prefs = await SharedPreferences.getInstance();
+  prefs.setString("username", _username);
+  main();
+}
