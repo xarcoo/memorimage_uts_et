f7397ba27cc541ade80794051e6f8827ef75463f
Styaaa <s160421048@student.ubaya.ac.id>
2024-04-12 23:12:34 +0800

    p valo

--- lib/game.dart
+++ lib/game.dart
@@ -13,8 +13,11 @@ class Game extends StatefulWidget {
 class _GameState extends State<Game> {
   bool animated = false;
   late Timer _timer;
+  late Timer _timerGambar;
   double opacityLev = 0;
   List<Question> _questions = [];
+  int i = 0;
+  int j = 0;
 
   @override
   void initState() {
@@ -26,10 +29,138 @@ class _GameState extends State<Game> {
     _timer = Timer.periodic(Duration(milliseconds: 1000), (timer) {
       setState(() {
         animated = true;
-        opacityLev = 0 + 1;
+      });
+    });
+
+    _timerGambar = Timer.periodic(Duration(milliseconds: 1000), (timer) {
+      setState(() {
+        if(opacityLev == 0){
+          opacityLev = 1;
+          if(i == _questions.length - 1){
+            timer.cancel();
+            opacityLev = 0;
+          }
+          else{
+            i++;
+
+          }
+        }
+        else if(opacityLev == 1){
+          opacityLev = 0;
+        }
       });
     });
   }
+
+  List<Widget> gambar(int i){
+    List<Widget> temp = [];
+
+    Widget a = TextButton(
+      child: Image.network(_questions[i].option_a),
+      onPressed: () {
+        setState(() {
+          checkAnswer(_questions[i].option_a);
+        });
+      },
+    );
+    Widget b = TextButton(
+      child: Image.network(_questions[i].option_b),
+      onPressed: () {
+        setState(() {
+          checkAnswer(_questions[i].option_b);
+        });
+      },
+    );
+    Widget c = TextButton(
+      child: Image.network(_questions[i].option_c),
+      onPressed: () {
+        setState(() {
+          checkAnswer(_questions[i].option_c);
+        });
+      },
+    );
+    Widget d = TextButton(
+      child: Image.network(_questions[i].option_d),
+      onPressed: () {
+        setState(() {
+          checkAnswer(_questions[i].option_d);
+        });
+      },
+    );
+
+    temp.add(a);
+    temp.add(b);
+    temp.add(c);
+    temp.add(d);
+
+    return temp;
+  }
+
+  void checkAnswer(String answer){
+
+    setState(() {
+      if (answer == _questions[j].answer) {
+        showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: Text('Quiz'),
+              content: Text('Betul'),
+              actions: <Widget>[
+                TextButton(
+                    onPressed: () {
+                      // Navigator.push(context, MaterialPageRoute(builder: (context) => TopPlayer()));
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('OK')
+                )
+              ],
+            )
+        );
+      }
+      else{
+        showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: Text('Quiz'),
+              content: Text('Salah'),
+              actions: <Widget>[
+                TextButton(
+                    onPressed: () {
+                      // Navigator.push(context, MaterialPageRoute(builder: (context) => TopPlayer()));
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('OK')
+                )
+              ],
+            )
+        );
+      }
+      
+      if(j < questions.length - 1){
+        j++;
+      }
+      else{
+        showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: Text('Quiz'),
+              content: Text('Tamat'),
+              actions: <Widget>[
+                TextButton(
+                    onPressed: () {
+                      // Navigator.push(context, MaterialPageRoute(builder: (context) => TopPlayer()));
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('OK')
+                )
+              ],
+            )
+        );
+      }
+    });
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -51,13 +182,20 @@ class _GameState extends State<Game> {
               )),
           Container(
               width: 250.0,
-              height: 250.0,
+              height: _timerGambar.isActive ? 250.0 : 0,
               child: AnimatedOpacity(
                 opacity: opacityLev,
                 duration: const Duration(seconds: 1),
-                child: Image.network(
-                    'https://loremflickr.com/320/240/cake?lock=9'),
-              )),
+                child: Image.network(questions[i].answer),
+              )
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+              child: GridView.count(
+                crossAxisCount: 2,
+                children: gambar(j),
+              )
+          ),
         ],
       ),
       appBar: AppBar(
